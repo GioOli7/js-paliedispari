8d9432e0a77b5eab6ca4fd270ba109765628bddb
GioOli7 <gio07live@gmail.com>
2021-03-29 15:43:02 +0200

    pari dispari

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
+/*
 var word = prompt('Inserisci una parola, ti dirò se è palindroma');
 if (isPalindroma(word)) {
     console.log(`La parola ${word} è palidroma`);
 } else {
     console.log(`la parola ${word} non è palindroma`);
 }
+*/
 
 ////////FUNZIONE
 /**
@@ -15,6 +17,8 @@ if (isPalindroma(word)) {
  * @param {string} parola parola da verificare
  * @returns true se è palindroma
  */
+
+/*
 function isPalindroma (parola) {
     var palindroma = '';
     for (var i = parola.length - 1; i >= 0; i--) {
@@ -26,9 +30,11 @@ function isPalindroma (parola) {
     }
     return false;
 }
+*/
+
 // avrei potuto inserire il risultato del log direttamente nella funzione, ma trovo più flessibile gestirlo
 // nel codice, ed avere un semplice true o false come risultato della funzione
-
+//*************************************************************************************************************** */
 
 
 
@@ -39,3 +45,52 @@ function isPalindroma (parola) {
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
+
+
+var PariDispariUser = prompt('Pari o dispari?\nDigita la parola corrispondente').toLowerCase().trim();
+console.log(`L'utente ha scelto ${PariDispariUser}`);
+var numUser = parseInt(prompt('Scegli un numero da 1 a 5'));
+console.log(`L'utente ha scelto il numero ${numUser}`);
+var numCPU = randomCPU();
+console.log(`Il numero casuale per il computer è ${numCPU}`);
+var somma = sum(numUser, numCPU);
+console.log(`La somma dei due valori è ${somma}`);
+if (PariDispariUser === isEven(somma)) {
+    console.log(`La somma è ${isEven(somma)}, quindi l'utente ha vinto!`);
+} else {
+    console.log(`La somma è ${isEven(somma)}, quindi l'utente ha perso!`);
+}
+
+
+
+/**
+ * Genera un numro casuale da 1 a 5
+ * @returns numero casuale da 1 a 5
+ */
+function randomCPU() {
+    rand = Math.floor(Math.random()*5) + 1;
+    return rand;
+}
+
+/**
+ * Esegue la somma tra due numeri
+ * @param {number} num1 primo addendo
+ * @param {number} num2 secondo addendo
+ * @returns somma dei due numeri
+ */
+function sum(num1, num2) {
+    sum = num1 + num2;
+    return sum;
+}
+
+/**
+ * Verifica se il numero è pari o dispari
+ * @param {number} numero numero da verificare
+ * @returns pari se il numero è pari, dispari se è dispari
+ */
+function isEven(numero) {
+    if (numero % 2 === 0) {
+        return 'pari';
+    }
+    return 'dispari';
+}
